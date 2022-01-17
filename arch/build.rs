9076d4bc9726d272b8ed0e6cb7d99618fbdce114
Dzenan Jupic <56133904+DzenanJupic@users.noreply.github.com>
2022-01-17 03:07:38 +0100

    fixed arch/build.rs
    Signed-off-by: Dzenan Jupic <56133904+DzenanJupic@users.noreply.github.com>

--- arch/build.rs
+++ arch/build.rs
@@ -1,9 +1,13 @@
 use std::env::var;
+use std::path::Path;
 
 fn main() {
-    let linker_file = format!("src/{}/link.ld", var("CARGO_CFG_TARGET_ARCH").unwrap());
+    let linker_file = format!("src/raw/{}/link.ld", var("CARGO_CFG_TARGET_ARCH").unwrap());
 
     println!("cargo:rerun-if-env-changed=CARGO_CFG_TARGET_ARCH");
-    println!("cargo:rerun-if-changed={}", linker_file);
     println!("cargo:rerun-if-changed=build.rs");
+
+    if Path::new(&linker_file).is_file() {
+        println!("cargo:rerun-if-changed={}", linker_file);
+    }
 }
